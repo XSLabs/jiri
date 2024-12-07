85baffa42e45e3aed473e9a4f418c092e7bb3132
Oliver Newman <olivernewman@google.com>
2024-12-07 01:02:21 +0000

    [tests] Add integration test for remote imports
    This test checks that Jiri supports importing a manifest from a remote
    repository that places the remote repository itself at the root of the
    checkout.
    
    This validates the theory that we could potentially move the public Jiri
    manifests into fuchsia.git and have fuchsia.git become an <import> in
    integration.git, so the pinned versions of dependencies listed in those
    manifests can be updated atomically with fuchsia.git to avoid breaking
    changes.
    
    Change-Id: I9acb2db6a50f64ecf0030b28243b1ec71d7239c3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/jiri/+/1170092
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Oliver Newman <olivernewman@google.com>
    Reviewed-by: Danielle Kay <danikay@google.com>

--- integrationtests/update_test.go
+++ integrationtests/update_test.go
@@ -6,6 +6,7 @@ package integrationtests
 
 import (
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -76,5 +77,68 @@ func TestUpdateWithDirtyProject(t *testing.T) {
 
 	if diff := cmp.Diff(newContents, got); diff != "" {
 		t.Errorf("Wrong foo.txt contents after update (-want +got):\n%s", diff)
+	}
+}
+
+func TestImportRemoteManifest(t *testing.T) {
+	t.Parallel()
+
+	// Set up a remote repo containing a manifest that includes the repo itself
+	// at the root.
+	importedRemoteDir := t.TempDir()
+	setupGitRepo(t, importedRemoteDir, map[string]any{
+		"imported_manifest": project.Manifest{
+			Projects: []project.Project{
+				// The project itself.
+				{
+					Name:   "imported",
+					Remote: importedRemoteDir,
+					Path:   ".",
+				},
+			},
+		},
+		"foo.txt": "foo\n",
+	})
+	importedRevision := strings.TrimSpace(
+		runSubprocess(t, importedRemoteDir, "git", "rev-parse", "HEAD"))
+
+	// Set up a remote repo that imports the above repository at the root of the
+	// checkout.
+	remoteDir := t.TempDir()
+	setupGitRepo(t, remoteDir, map[string]any{
+		"top_level_manifest": project.Manifest{
+			Imports: []project.Import{
+				{
+					Name:     "imported",
+					Manifest: "imported_manifest",
+					Remote:   importedRemoteDir,
+					Revision: importedRevision,
+					Root:     "",
+				},
+			},
+			Projects: []project.Project{
+				{
+					Name:   "manifest",
+					Remote: remoteDir,
+					Path:   "manifest",
+				},
+			},
+		},
+	})
+
+	root := t.TempDir()
+	jiri := jiriInit(t, root)
+	jiri("import", "top_level_manifest", remoteDir)
+	jiri("update")
+
+	wantFiles := []string{
+		"foo.txt",
+		"imported_manifest",
+		"manifest/top_level_manifest",
+	}
+
+	gotFiles := listDirRecursive(t, root)
+	if diff := cmp.Diff(wantFiles, gotFiles); diff != "" {
+		t.Errorf("Wrong directory contents after update (-want +got):\n%s", diff)
 	}
 }
