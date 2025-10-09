XSLabs/jiri

commit 4d3c09ca88ccaed70231132642ba808eebc5e8fb

[jiri] Show CIPD progress bars by default
The CIPD tool, when invoked at its default log level, will print
progress bars as it fetches packages. This is particularly useful
for the current integration checkout, which typically requires multiple
minutes to fetch all the CIPD packages, even on a cloudtop.

When CIPD is invoked at the "warning" log level, nothing is printed.

This CL modifies our invocation of CIPD such that:
  - `jiri` -> CIPD default log level, with progress bars
  - `jiri -q` -> CIPD warning log level, typically no output
  - `jiri -v` or `-vv` -> CIPD debug log level

Change-Id: I0396c1d9b58074a641f399476842173f4041f315
Reviewed-on: https://fuchsia-review.googlesource.com/c/jiri/+/1263784
Reviewed-by: Oliver Newman <olivernewman@google.com>
Commit-Queue: Marc Khouri <mnck@google.com>

cipd/cipd.go
@@ -403,10 +403,12 @@ func Ensure(jirix *jiri.X, file, projectRoot string, timeout uint) error {
 		"-max-threads", strconv.Itoa(jirix.CipdMaxThreads),
 	}
 
-	// If jiri is *not* running with -v, use the less verbose cipd "warning"
-	// log-level.
-	if jirix.Logger.LoggerLevel < log.DebugLevel {
+	if (jirix.Logger.LoggerLevel <= log.WarningLevel) || (!jirix.Logger.IsProgressEnabled()) {
+		// If jiri is running with -quiet or -show-progess=false, use cipd's "warning" log-level.
 		args = append(args, "-log-level", "warning")
+	} else if jirix.Logger.LoggerLevel >= log.DebugLevel {
+		// If jiri is running with -v or louder, use cipd's "debug" log-level.
+		args = append(args, "-log-level", "debug")
 	}
 
 	task := jirix.Logger.AddTaskMsg("Fetching CIPD packages")
@@ -437,10 +439,13 @@ func EnsureFileVerify(jirix *jiri.X, file string) error {
 		"ensure-file-verify",
 		"-ensure-file", file,
 	}
-	// If jiri is *not* running with -v, use the less verbose cipd "warning"
-	// log-level.
-	if jirix.Logger.LoggerLevel < log.DebugLevel {
+
+	if (jirix.Logger.LoggerLevel <= log.WarningLevel) || (!jirix.Logger.IsProgressEnabled()) {
+		// If jiri is running with -quiet or -show-progess=false, use cipd's "warning" log-level.
 		args = append(args, "-log-level", "warning")
+	} else if jirix.Logger.LoggerLevel >= log.DebugLevel {
+		// If jiri is running with -v or louder, use cipd's "debug" log-level.
+		args = append(args, "-log-level", "debug")
 	}
 
 	task := jirix.Logger.AddTaskMsg("Verifying CIPD ensure file")

commit b8bb59c6ba5f3d5d1d6d7174e978c19bd317b653

[cipd] Fix CIPD verbose logging
Fixes a regression from commit 4d3c09ca88ccaed70231132642ba808eebc5e8fb.

Prior to that commit, if Jiri was invoked with the `-vv` flag it would
always run the cipd CLI with the default log level ("info").

However, that commit made Jiri run cipd with `-log-level=warning` (which
is much less verbose than `-log-level=info`) if progress bars were
disabled, as they implicitly are when running in infra because stdout is
not a terminal, such as in infra. So cipd CLI produced much less output
when invoked by Jiri in infra.

This change fixes that, so `jiri -vv` always runs CIPD with
`-log-level=debug` (which is even more verbose than the old "info"
level), even when progress bars are disabled either explicitly or
implicitly. cipd progress bars are still disabled when they should be,
but by using the `CIPD_SIMPLE_TERMINAL_UI` env var that the cipd CLI
supports instead of by using the `-log-level` flag.

Change-Id: I7f1f7d2f86af5cb4c75a2ee3710fc89aafc48b4a
Reviewed-on: https://fuchsia-review.googlesource.com/c/jiri/+/1388053
Commit-Queue: Oliver Newman <olivernewman@google.com>
Fuchsia-Auto-Submit: Oliver Newman <olivernewman@google.com>
Reviewed-by: Ina Huh <ihuh@google.com>

cipd/cipd.go
@@ -403,12 +403,22 @@ func Ensure(jirix *jiri.X, file, projectRoot string, timeout uint) error {
 		"-max-threads", strconv.Itoa(jirix.CipdMaxThreads),
 	}
 
-	if (jirix.Logger.LoggerLevel <= log.WarningLevel) || (!jirix.Logger.IsProgressEnabled()) {
-		// If jiri is running with -quiet or -show-progess=false, use cipd's "warning" log-level.
+	if jirix.Logger.LoggerLevel <= log.WarningLevel {
+		// If jiri is running with -quiet, use cipd's "warning" log-level.
 		args = append(args, "-log-level", "warning")
 	} else if jirix.Logger.LoggerLevel >= log.DebugLevel {
 		// If jiri is running with -v or louder, use cipd's "debug" log-level.
 		args = append(args, "-log-level", "debug")
+	}
+
+	env := os.Environ()
+	// Add User-Agent info for cipd
+	env = append(env, "CIPD_HTTP_USER_AGENT_PREFIX="+getUserAgent())
+	if !jirix.Logger.IsProgressEnabled() {
+		// Force CIPD to use the simple UI (without progress bars) when Jiri
+		// progress bars are disabled. See
+		// https://chromium.googlesource.com/infra/luci/luci-go/+/59c5d4cd5499a35251cad9e0bcad659766f0e411/cipd/client/cli/main.go#70
+		env = append(env, "CIPD_SIMPLE_TERMINAL_UI=1")
 	}
 
 	task := jirix.Logger.AddTaskMsg("Fetching CIPD packages")
@@ -417,8 +427,7 @@ func Ensure(jirix *jiri.X, file, projectRoot string, timeout uint) error {
 
 	// Construct arguments and invoke cipd for ensure file
 	command := exec.CommandContext(ctx, jirix.CIPDPath(), args...)
-	// Add User-Agent info for cipd
-	command.Env = append(os.Environ(), "CIPD_HTTP_USER_AGENT_PREFIX="+getUserAgent())
+	command.Env = env
 	command.Stdin = os.Stdin
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr